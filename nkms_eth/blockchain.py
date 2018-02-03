20621152d17c37bce7ed53ac4944957d657296ac
Kieran R Prasch <kieranprasch@gmail.com>
2018-02-03 13:50:56 -0800

    Refactors blockchain into a threadlocal class instance.

--- nkms_eth/blockchain.py
+++ nkms_eth/blockchain.py
@@ -4,46 +4,46 @@ import nkms_eth
 import appdirs
 from os.path import dirname, join, abspath
 
-DEFAULT_NETWORK = 'mainnetrpc'
-PYTHON_PROJECT_NAME = 'nucypher-kms'
-TIMEOUT = 60
-_project = threading.local()
 
-# This config is persistent and is created in user's .local directory
-REGISTRAR_PATH = join(appdirs.user_data_dir(PYTHON_PROJECT_NAME),
-                      'registrar.json')
+class Blockchain(object):
+    network = 'mainnetrpc'
+    project_name = 'nucypher-kms'
+    _project = threading.local()
+    registrar_path = join(appdirs.user_data_dir(project_name), 'registrar.json')    # Persistent; In user's .local dir
 
+    def __init__(self, project_name='nucypher-kms', timeout=60):
+        self.project_name = project_name
+        self.timeout = timeout
 
-def project():
-    # Hardcoded the config for the project
-    # It will read user-specific configs also which may override it
-    if not hasattr(_project, 'project'):
         project_dir = join(dirname(abspath(nkms_eth.__file__)), 'project')
-        project = populus.Project(project_dir)
-        project.config['chains.mainnetrpc']['contracts']['backends']['JSONFile']['settings']['file_path'] = REGISTRAR_PATH
-        _project.project = project
-    return _project.project
+        project = populus.Project(project_dir)  # setter
+        project.config['chains.mainnetrpc']['contracts']['backends']['JSONFile']['settings']['file_path'] = self.registrar_path
+
+        self._project.project = project
+
+    def __repr__(self):
+        class_name = self.__class__.__name__
+        return f"{class_name}(network={self.network}, project_name={self.project_name}, timeout={self.timeout})"
+
+    def __str__(self):
+        return f"{self.__class__.__name__} {self.network}:{self.project_name}"
+
+    def __enter__(self):
+        chain = self._project.project.get_chain(self.network)
+        self._project.chain = chain.__enter__()
+        return self._project.chain
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._project.chain.__exit__(None, None, None)
+
+    def __del__(self):
+        for attr in ('project', 'chain', 'w3'):
+            if hasattr(self._project, attr):
+                delattr(self._project, attr)
 
 
-def get_chain(name=None):
-    return project().get_chain(name or DEFAULT_NETWORK)
+class TesterBlockchain(Blockchain):
+    network = 'tester'
 
-
-def chain(name=None):
-    if not hasattr(_project, 'chain'):
-        _project.chain = get_chain(name).__enter__()
-    return _project.chain
-
-
-def disconnect():
-    _project.chain.__exit__(None, None, None)
-    if hasattr(_project, 'project'):
-        delattr(_project, 'project')
-    if hasattr(_project, 'chain'):
-        delattr(_project, 'chain')
-    if hasattr(_project, 'web3'):
-        delattr(_project, 'web3')
-
-
-def web3():
-    return chain().web3
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
