c33d31b086792b375666ca0ce71816c18b1ae50a
Kieran R Prasch <kieranprasch@gmail.com>
2018-02-04 19:38:38 -0800

    Creates Blockchain class to interface with populus and local chain.

--- nkms_eth/blockchain.py
+++ nkms_eth/blockchain.py
@@ -5,41 +5,50 @@ import appdirs
 from os.path import dirname, join, abspath
 
 
-class Blockchain(object):
+class Blockchain:
     network = 'mainnetrpc'
     project_name = 'nucypher-kms'
     _project = threading.local()
     registrar_path = join(appdirs.user_data_dir(project_name), 'registrar.json')    # Persistent; In user's .local dir
+
+    class NotEnoughUrsulas(Exception):
+        pass
 
     def __init__(self, project_name='nucypher-kms', timeout=60):
         self.project_name = project_name
         self.timeout = timeout
 
         project_dir = join(dirname(abspath(nkms_eth.__file__)), 'project')
-        project = populus.Project(project_dir)  # setter
+        project = populus.Project(project_dir)
         project.config['chains.mainnetrpc']['contracts']['backends']['JSONFile']['settings']['file_path'] = self.registrar_path
 
+        self.project_dir = project_dir
         self._project.project = project
+        self._project.chain = project.get_chain(self.network)
 
     def __repr__(self):
         class_name = self.__class__.__name__
-        return f"{class_name}(network={self.network}, project_name={self.project_name}, timeout={self.timeout})"
+        r = "{}(network={}, project_name={}, timeout={})"
+        return r.format(class_name, self.network, self.project_name, self.timeout)
 
     def __str__(self):
         return f"{self.__class__.__name__} {self.network}:{self.project_name}"
 
     def __enter__(self):
-        chain = self._project.project.get_chain(self.network)
-        self._project.chain = chain.__enter__()
-        return self._project.chain
+        return self._project.chain.__enter__()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self._project.chain.__exit__(None, None, None)
+        return self._project.chain.__exit__(None, None, None)
 
     def __del__(self):
         for attr in ('project', 'chain', 'w3'):
             if hasattr(self._project, attr):
                 delattr(self._project, attr)
+
+    def get_contract(self, name):
+        """ Gets an existing contract or returns an error """
+        with self as chain:
+            return chain.provider.get_contract(name)
 
 
 class TesterBlockchain(Blockchain):
--- nkms_eth/escrow.py
+++ nkms_eth/escrow.py
@@ -1,70 +1,72 @@
 import random
 from typing import List
-from nkms_eth import blockchain
-from nkms_eth import token
 
-ESCROW_NAME = 'Escrow'
-HOURS_PER_PERIOD = 1  # 24
-MIN_RELEASE_PERIODS = 1  # 30
-MAX_AWARDED_PERIODS = 365
-MINING_COEFF = [
-    HOURS_PER_PERIOD,
-    2 * 10 ** 7,
-    MAX_AWARDED_PERIODS,
-    MAX_AWARDED_PERIODS,
-    MIN_RELEASE_PERIODS
-]
-NULL_ADDR = '0x' + '0' * 40
+from nkms_eth.token import NuCypherKMSToken
+from .blockchain import Blockchain
 
 
-def create():
-    """
-    Creates an escrow which manages mining
-    """
-    chain = blockchain.chain()
-    creator = chain.web3.eth.accounts[0]  # TODO: make it possible to override
-    tok = token.get()
-    escrow, tx = chain.provider.get_or_deploy_contract(
-        ESCROW_NAME, deploy_args=[token.get().address] + MINING_COEFF,
-        deploy_transaction={'from': creator})
-    chain.wait.for_receipt(tx, timeout=blockchain.TIMEOUT)
-    tx = tok.transact({'from': creator}).addMiner(escrow.address)
-    chain.wait.for_receipt(tx, timeout=blockchain.TIMEOUT)
-    return escrow
+class Escrow:
+    escrow_name = 'Escrow'
+    hours_per_period = 1       # 24
+    min_release_periods = 1    # 30
+    max_awarded_periods = 365
+    mining_coeff = [
+        hours_per_period,
+        2 * 10 ** 7,
+        max_awarded_periods,
+        max_awarded_periods,
+        min_release_periods
+    ]
+    null_addr = '0x' + '0' * 40
 
+    def __init__(self, blockchain, token, contract=None):
 
-def get():
-    """
-    Returns an escrow object
-    """
-    return token.get(ESCROW_NAME)
+        with blockchain as chain:
+            creator = chain.web3.eth.accounts[0]  # TODO: make it possible to override
+            if not contract:
+                contract, txhash = chain.provider.deploy_contract(
+                    self.escrow_name,
+                    deploy_args=[token.contract.address]+self.mining_coeff,
+                    deploy_transaction={'from': creator})
 
+                chain.wait.for_receipt(txhash, timeout=blockchain.timeout)
+                txhash = token.contract.transact({'from': creator}).addMiner(contract.address)
+                chain.wait.for_receipt(txhash, timeout=blockchain.timeout)
 
-def sample(n: int = 10)-> List[str]:
-    """
-    Select n random staking Ursulas, according to their stake distribution
-    The returned addresses are shuffled, so one can request more than needed and
-    throw away those which do not respond
-    """
-    escrow = get()
-    n_select = int(n * 1.7)  # Select more ursulas
-    n_tokens = escrow.call().getAllLockedTokens()
-    duration = 10
+            self.contract = contract
 
-    for _ in range(5):  # number of tries
-        points = [0] + sorted(random.randrange(n_tokens) for _ in
-                              range(n_select))
-        deltas = [i - j for i, j in zip(points[1:], points[:-1])]
-        addrs = set()
-        addr = NULL_ADDR
-        shift = 0
+    def __call__(self, *args, **kwargs):
+        return self.contract.call()
 
-        for delta in deltas:
-            addr, shift = escrow.call().findCumSum(addr, delta + shift, duration)
-            addrs.add(addr)
+    @classmethod
+    def get(cls, blockchain, token):
+        """ Returns an escrow object """
+        contract = blockchain.get_contract(cls.escrow_name)
+        return cls(blockchain=blockchain, token=token, contract=contract)
 
-        if len(addrs) >= n:
-            addrs = random.sample(addrs, n)
-            return addrs
+    def sample(self, quantity: int = 10) -> List[str]:
+        """
+        Select n random staking Ursulas, according to their stake distribution.
+        The returned addresses are shuffled, so one can request more than needed and
+        throw away those which do not respond.
 
-    raise Exception('Not enough Ursulas')
+        """
+
+        n_select = round(quantity*1.7)  # Select more Ursulas
+        n_tokens = self().getAllLockedTokens()
+        duration = 10
+
+        for _ in range(5):  # number of tries
+            points = [0] + sorted(random.randrange(n_tokens) for _ in range(n_select))
+            deltas = [i-j for i, j in zip(points[1:], points[:-1])]
+
+            addrs, addr, shift = set(), self.null_addr, 0
+            for delta in deltas:
+                addr, shift = self().findCumSum(addr, delta+shift, duration)
+                addrs.add(addr)
+
+            if len(addrs) >= quantity:
+                addrs = random.sample(addrs, quantity)
+                return addrs
+        else:
+            raise Blockchain.NotEnoughUrsulas('Not enough Ursulas.')
