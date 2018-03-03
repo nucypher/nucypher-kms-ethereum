f4c9b1fe08b516fce3c0ee2d27d1e275d16a1649
Kieran Prasch <kieranprasch@gmail.com>
2018-03-02 21:08:30 -0800

    PolicyManager, Policy, and PolicyAuthor (client) classes

--- nkms_eth/policies.py
+++ nkms_eth/policies.py
@@ -1,11 +1,11 @@
-from typing import Tuple
+from typing import Tuple, List
 
 from nkms_eth.blockchain import Blockchain
 from nkms_eth.escrow import Escrow
 from nkms_eth.token import NuCypherKMSToken
 
 
-class Policy:
+class NetworkPolicy:
     def __init__(self, policy_id: bytes, client_addr: str, node_addr: str,
                  value: int, gas_price: int, duration: int):
         self._policy_id = policy_id
@@ -34,9 +34,13 @@ class PolicyManager:
         self.token = token
         self.escrow = escrow    # TODO: must be deployed
 
-        self._policies = []
+        self._policies = {}
         self.armed = False
         self.__contract = None
+
+    @property
+    def is_deployed(self):
+        return bool(self.__contract)
 
     def arm(self) -> None:
         self.armed = True
@@ -44,7 +48,7 @@ class PolicyManager:
     def deploy(self) -> Tuple[str, str]:
         if self.armed is False:
             raise PolicyManager.ContractDeploymentError('Contract not armed')
-        if self.__contract is not None:
+        if self.is_deployed is True:
             raise PolicyManager.ContractDeploymentError
 
         # Creator deploys the policy manager
@@ -71,28 +75,73 @@ class PolicyManager:
         """Transmit a network transaction."""
         return self.__contract.transact(*args)
 
-    def create_policy(self, policy_id: bytes, client_addr: str,
-                      node_addr: str, duration: int,
-                      value: int, gas_price: int) -> Policy:
+    def publish_policy(self, hrac: str, client_addr: str,
+                      miner_addr: str, duration: int,
+                      value: int, gas_price: int) -> NetworkPolicy:
 
         payload = {'from': client_addr,
                    'value': value,
                    'gas_price': gas_price}
 
-        txhash = self.transact(payload).createPolicy(policy_id,
-                                                     node_addr,
+        txhash = self.transact(payload).createPolicy(hrac,
+                                                     miner_addr,
                                                      duration)
 
         self.blockchain._chain.wait.for_receipt(txhash)
 
-        policy = Policy(policy_id, client_addr, node_addr, duration)
-        self._policies.append(policy)    # Track this policies
+        policy = NetworkPolicy(hrac, client_addr, miner_addr, duration)
+        self._policies.update({hrac: policy})    # Track this policies
 
         return policy
 
-    def get_policy(self, policy_id: bytes) -> Policy:
+    def fetch_policy(self, policy_id: bytes) -> NetworkPolicy:
         client_addr, node_addr, rate, *periods = self.__contract.call().policies(policy_id)
-        policy = Policy(policy_id, client_addr, node_addr, rate, duration=len(periods))
+        policy = NetworkPolicy(policy_id, client_addr, node_addr, rate, duration=len(periods))
         return policy
 
+    def
+
+
+class PolicyAuthor:
+    def __init__(self, address: bytes, policy_manager: PolicyManager):
+
+        if policy_manager.is_deployed is False:
+            raise PolicyManager.ContractDeploymentError('PolicyManager contract not deployed.')
+        self.policy_manager = policy_manager
+
+        self.policies = dict()
+        try:
+            # Check for existing records for this address
+            self.policies = self.policy_manager._policies[address]
+        except KeyError:
+            # Otherwise create a new record for this address
+            self.policy_manager._policies[address] = self.policies
+
+        if isinstance(address, bytes):
+            address = address.hex()
+        self.address = address
+
+    def create_policy(self, hrac: bytes, miner: str, duration: int,
+                      value: int, gas_price: int) -> NetworkPolicy:
+
+        if isinstance(hrac, bytes):
+            hrac = hrac.hex()
+
+        policy = self.policy_manager.publish_policy(hrac, self.address,
+                                                    miner, duration,
+                                                    value, gas_price)
+        self.policies[hrac] = policy
+        return policy
+
+    def revoke_policy(self, hrac):
+        try:
+            policy = self.policies[hrac]
+        except KeyError:
+            raise Exception('No such policy')
+        else:
+            self.policy_manager.revoke(hrac)
+
+    def select_ursulas(self, quantity: int) -> List[str]:
+        ursulas = self.policy_manager.escrow.sample(quantity=quantity)
+        return ursulas
 
