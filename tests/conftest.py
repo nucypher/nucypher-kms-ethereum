ccb901eb671229e509d52b2d764d6a746e6a188d
Kieran Prasch <kieranprasch@gmail.com>
2018-03-16 03:44:40 -0700

    Test module reorg, and update to contract / deployers API

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,28 +15,30 @@ def testerchain():
 
 
 @pytest.fixture(scope='function')
-def token_deployer(testerchain):
-    token_deployer = NuCypherKMSTokenDeployer(blockchain=testerchain)
+def mock_token_deployer(testerchain):
+    token_deployer = MockNuCypherKMSTokenDeployer(blockchain=testerchain)
     token_deployer.arm()
     token_deployer.deploy()
     yield token_deployer
 
 
 @pytest.fixture(scope='function')
-def escrow_deployer(token_agent):
+def mock_miner_escrow_deployer(token_agent):
     escrow = MockMinerEscrowDeployer(token_agent)
     escrow.arm()
     escrow.deploy()
     yield escrow
 
 
+# Unused args preserve fixture dependency order #
+
 @pytest.fixture(scope='function')
-def token_agent(testerchain):
+def token_agent(testerchain, mock_token_deployer):
     token = NuCypherKMSTokenAgent(blockchain=testerchain)
     yield token
 
 
 @pytest.fixture(scope='function')
-def miner_agent(token_agent):
+def miner_agent(token_agent, mock_token_deployer, mock_miner_escrow_deployer):
     miner_agent = MinerAgent(token_agent)
     yield miner_agent
